74de886abda6a818c8fcbc6d8a95100a8b30d1a5
RosarioRu <mariarruval@gmail.com>
2022-09-13 19:08:55 -0700

    Prepare models for linking with api.
    Co-authored-by: Adrienne Matosich <amatosich@gmail.com>
    Co-authored-by: Ryan Broadsword <rbroadsword@gmail.com>
    Co-authored-by: Sean Julius <crimsonsand@live.com>

--- BookwormClient/Models/Book.cs
+++ BookwormClient/Models/Book.cs
@@ -7,18 +7,30 @@ namespace BookwormClient.Models
 {
   public class Book
   {
+
     public int BookId { get; set; }
     public string Title { get; set; }
     public string Author { get; set; }
-    public string Category { get; set; }
+    public string AgeRange { get; set; }
     public string Summary { get; set; }
+    public string Rating { get; set; }
     public string Genre { get; set; }
     public string Tags { get; set; }
-    public string AgeRange { get; set; } //might be a way to use a numeric data type here
-    public List<string> Reviews { get; set; }
+    public string Reviews { get; set; }
+    
+    //-------------------------------------------------------------------------
+    // public int BookId { get; set; }
+    // public string Title { get; set; }
+    // public string Author { get; set; }
+    // public string Category { get; set; }
+    // public string Summary { get; set; }
+    // public string Genre { get; set; }
+    // public string Tags { get; set; }
+    // public string AgeRange { get; set; } //might be a way to use a numeric data type here
+    // public List<string> Reviews { get; set; }
 
-    public int Rating { get; set; } //we need a list of all ratings + average of them - 2 dimensional array? Or a list with a seperate method for averaging list?
-
+    // public int Rating { get; set; } //we need a list of all ratings + average of them - 2 dimensional array? Or a list with a seperate method for averaging list?
+    //---------------------------------------------------------------------------
 
     public static List<Book> GetBooks()
     {
@@ -30,5 +42,5 @@ namespace BookwormClient.Models
 
       return BookList;
     }
-}
+  }
 }
