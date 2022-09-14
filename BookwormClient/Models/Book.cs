5c69dd3fd4154e1875553406ec79f3f1be684703
RosarioRu <mariarruval@gmail.com>
2022-09-13 18:50:42 -0700

    WIP: properties for Book class.

--- BookwormClient/Models/Book.cs
+++ BookwormClient/Models/Book.cs
@@ -15,41 +15,9 @@ namespace BookwormClient.Models
     public string Genre { get; set; }
     public string Tags { get; set; }
     public string AgeRange { get; set; } //might be a way to use a numeric data type here
-    public list<string> Reviews { get; set; }
+    public List<string> Reviews { get; set; }
 
     public int Rating { get; set; } //we need a list of all ratings + average of them - 2 dimensional array? Or a list with a seperate method for averaging list?
-
-    // public list<int> Ratings { get; set}
-
-    // public static RatingAverage()
-    // {
-    //   //average the int(s) in list called Ratings
-    //   return average;
-    // }
-
-
-//     rating [[4], [3.5]]
-//     first index: single rating, second index: average
-
-//     int[] ratingArray = new int[5] {
-//       1,
-//       2,
-//       3, 
-//       4,
-//       5
-//     };
-//     int length = ratingArray.Length; /// length = 1
-//     int sum = 0;
-//     int average = 0;
-//     for (int i =0; i < length; i++) {
-//       sum += ratingArray[i];
-//     }
-//     average = sum / length;
-// Console.Writeline("Sum = " + sum);
-// Console.Writeline("Average Of integer elements = " + average);
-
-
-  }
 
 
     public static List<Book> GetBooks()
@@ -62,5 +30,5 @@ namespace BookwormClient.Models
 
       return BookList;
     }
-  
+}
 }
