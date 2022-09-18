95edd0bc5d3a161709007e95830f79a7ceadac50
RosarioRu <mariarruval@gmail.com>
2022-09-18 14:51:29 -0700

    Added proxies for lazy loading
    Co-authored-by: Adrienne Matosich <amatosich@gmail.com>
    Co-authored-by: Ryan Broadsword <rbroadsword@gmail.com>
    Co-authored-by: Sean Julius

--- BookwormClient/Startup.cs
+++ BookwormClient/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-<<<<<<< HEAD
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-=======
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
->>>>>>> a386147d402209de782c47775d94dc599db81891
 using BookwormClient.Models;
 using Microsoft.AspNetCore.Identity;
 
@@ -17,14 +11,6 @@ namespace BookwormClient
 {
   public class Startup
   {
-<<<<<<< HEAD
-    public Startup(IWebHostEnvironment env)
-    {
-      var builder = new ConfigurationBuilder()
-        .SetBasePath(env.ContentRootPath)
-        .AddJsonFile("appsettings.json");
-      Configuration = builder.Build();
-=======
     public Startup(IWebHostEnvironment env) //authentication startup
     {
       var builder = new ConfigurationBuilder()
@@ -77,95 +63,6 @@ namespace BookwormClient
         await context.Response.WriteAsync("Oops, something went wrong!");
       });
     }
-        //previous startup
-        /*public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }*/
->>>>>>> a386147d402209de782c47775d94dc599db81891
-    }
-
-    public IConfigurationRoot Configuration { get; set; }
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddMvc();
-
-      services.AddEntityFrameworkMySql()
-        .AddDbContext<BookwormClientContext>(options => options
-        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
-        
-      services.AddIdentity<ApplicationUser, IdentityRole>()
-        .AddEntityFrameworkStores<BookwormClientContext>()
-        .AddDefaultTokenProviders();
-
-      services.Configure<IdentityOptions>(options =>
-      {
-        options.Password.RequireDigit = false;
-        options.Password.RequiredLength = 0;
-        options.Password.RequireLowercase = false;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequiredUniqueChars = 0;
-      });
-    }
-
-    public void Configure(IApplicationBuilder app)
-    {
-      app.UseDeveloperExceptionPage();
-
-      app.UseAuthentication(); 
-
-      app.UseRouting();
-
-      app.UseAuthorization();
-
-      app.UseEndpoints(routes =>
-      {
-        routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-      });
-
-      app.UseStaticFiles();
-      
-      app.Run(async (context) =>
-      {
-        await context.Response.WriteAsync("Oops, something went wrong!");
-      });
-    }
   }
 }
+        
