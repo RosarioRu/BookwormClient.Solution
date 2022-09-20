afec58f39c402d6fa5c74bd011bc42b6e7428e00
Ryan Broadsword <rbroadsword@gmail.com>
2022-09-19 18:36:17 -0700

    got Books and Librarys Controllers working

--- BookwormClient/Controllers/BooksController.cs
+++ BookwormClient/Controllers/BooksController.cs
@@ -31,34 +31,83 @@ namespace BookwormClient.Controllers
       return View(userBooks); 
     }
 
-    // [Authorize]
-    // public ActionResult Create()
-    // {
-    //   return View(); 
-    // }
+    [Authorize]
+    public ActionResult Create()
+    {
+      return View(); 
+    }
 
-    // [HttpPost]
-    // public async Task<ActionResult> Create(Book book, int LibraryId)
-    // {
-    //   var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value; 
-    //   var currentUser = await _userManager.FindByIdAsync(userId); 
-    //   book.User = currentUser; 
-    //   _db.Books.Add(book); 
-    //   _db.SaveChanges(); 
-    //   if (LibraryId != 0)
-    //   {
-    //     _db.BookLibrarys.Add(new BookLibrary() {LibraryId = LibraryId, BookId = book.BookId}); 
-    //   }
-    //   _db.SaveChanges(); 
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public async Task<ActionResult> Create(Book book, int LibraryId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value; 
+      var currentUser = await _userManager.FindByIdAsync(userId); 
+      book.User = currentUser; 
+      _db.Books.Add(book); 
+      _db.SaveChanges(); 
+      if (LibraryId != 0)
+      {
+        _db.BookLibrary.Add(new BookLibrary() {LibraryId = LibraryId, BookId = book.BookId}); 
+      }
+      _db.SaveChanges(); 
+      return RedirectToAction("Index");
+    }
 
-    // public async Task<ActionResult> Details(int id)
-    // {
-    //   var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value; 
-    //   var currentUser = await _userManager.FindByIdAsync(userId);
-    //   Book thisBook = _db.Books.FirstOrDefault(book => book.BookId == id); 
-    //   return View(thisBook); 
-    // }
+    public async Task<ActionResult> Details(int id)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value; 
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      Book thisBook = _db.Books.FirstOrDefault(book => book.BookId == id); 
+      return View(thisBook); 
+    }
+
+    [Authorize]
+    public ActionResult Edit(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id); 
+      return View(thisBook); 
+    }
+
+    [Authorize]
+    [HttpPost]
+    public ActionResult Edit(Book book)
+    {
+      _db.Entry(book).State = EntityState.Modified; 
+      _db.SaveChanges(); 
+      return RedirectToAction("Index"); 
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      return View(thisBook); 
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id);
+      _db.Books.Remove(thisBook); 
+      _db.SaveChanges(); 
+      return RedirectToAction("Index"); 
+    }
+
+    public ActionResult AddLibrary(int id)
+    {
+      var thisBook = _db.Books.FirstOrDefault(book => book.BookId == id); 
+      ViewBag.LibraryId = new SelectList(_db.Librarys, "LibraryId", "Name"); 
+      return View(thisBook); 
+    }
+
+    [HttpPost]
+    public ActionResult AddLibrary(Book book, int LibraryId)
+    {
+      if (LibraryId != 0)
+      {
+        _db.BookLibrary.Add(new BookLibrary() { LibraryId = LibraryId, BookId = book.BookId}); 
+        _db.SaveChanges(); 
+      }
+      return RedirectToAction("Index");
+    }
   }
 }
